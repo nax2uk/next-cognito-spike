1d28581e2ce84e81e2b14d945eadee1f58da3a61
nax2uk <azlinayeo@gmail.com>
2021-04-19 12:14:42 +0100

    installed gov.uk frontend

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b67a4aa290d3189dcbc1";
+/******/ 	var hotCurrentHash = "42572bd59437df91bf72";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,19 @@
 
 const HomePage = () => {
-    return <div>Welcome to Next.js!</div>
+    return (
+        <main>
+            <h1>Sign in to your account</h1>
+            <form>
+            <label>email</label>
+            <input type="text"></input>
+            <label>password</label>
+            <input type="password"></input>
+            <button type="submit">Login</button>
+            </form>
+            
+        </main>
+    
+    )
 }
 
 export default HomePage;
